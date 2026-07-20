f27f2c905a5bbc3e35db6f6156fa506ac1fe566b
Jerry Yarnetsky <36451755+yarnetsky@users.noreply.github.com>
2026-07-20 20:16:56 +0000

    hiding blog posts

--- src/consts.ts
+++ src/consts.ts
@@ -20,6 +20,6 @@ export const NAV_ITEMS = [
   { href: '/', label: 'Home' },
   { href: '/about/', label: 'About' },
   { href: '/projects/', label: 'Projects' },
-  { href: '/blog/', label: 'Notes' },
+  // { href: '/blog/', label: 'Notes' },
   { href: '/search/', label: 'Search' },
 ] as const;
